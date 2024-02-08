c4d88172298481fad59e0439682a3ce5c2bd0e0b
strn18 <strn18@korea.ac.kr>
2024-02-08 15:44:55 +0900

    MOD: About 페이지 추가에 따른 라우터 연결

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { RegisterPage } from '../pages/Register';
 import { MainPage } from '../pages/Main';
 import { ListPage } from '../pages/List';
+import { AboutPage } from '../pages/About';
 
 /**
  * 어느 url에 어떤 페이지를 보여줄지 정해주는 컴포넌트입니다.
@@ -12,6 +13,7 @@ export function RouteComponent() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/about" element={<AboutPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/list/:age" element={<ListPage />} />
     </Routes>
