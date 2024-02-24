57b83737e05ffde74b59c8cac6c4fd957c80ef6c
strn18 <strn18@korea.ac.kr>
2024-02-24 19:55:50 +0900

    MOD: TopNav의 버튼 호버링 시 색 변화 추가, 머무르는 페이지에 따른 초록색 바 추가(완성)

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -1,19 +1,33 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
-import { Button } from '@mui/material';
+import { Typography } from '@mui/material';
 
 export default function TopNav() {
   const navigate = useNavigate();
+  const [curPage, setCurPage] = useState(0); // 0: MainPage, 1: AboutPage
 
   return (
-    <Box paddingX={1} sx={{ width: '100%', height: '5%', backgroundColor: 'black' }}>
-      <Box>
-        <Button sx={{ textTransform: 'none' }} onClick={() => navigate('/about')}>
-          About When2meet
-        </Button>
-        <Button sx={{ textTransform: 'none' }} onClick={() => alert('Go To Main Page')}>
-          Plan a New Event
-        </Button>
+    <Box display={'flex'} paddingX={1} sx={{ width: '100%', height: '5%', backgroundColor: 'black' }}>
+      <Box
+        sx={{ color: '#DEDEDE', ':hover': { backgroundColor: '#DEDEDE', color: 'black', cursor: 'pointer' } }}
+        onClick={() => {
+          setCurPage(1);
+          navigate('/about');
+        }}
+      >
+        <Box sx={{ height: '12%', width: '100%', backgroundColor: curPage === 1 ? '#00FF00' : null }}></Box>
+        <Typography marginX={1.5}>About When2meet</Typography>
+      </Box>
+      <Box
+        sx={{ color: '#DEDEDE', ':hover': { backgroundColor: '#DEDEDE', color: 'black', cursor: 'pointer' } }}
+        onClick={() => {
+          setCurPage(0);
+          navigate('/');
+        }}
+      >
+        <Box sx={{ height: '12%', width: '100%', backgroundColor: curPage === 0 ? '#00FF00' : null }}></Box>
+        <Typography marginX={1.5}>Plan a New Event</Typography>
       </Box>
     </Box>
   );
